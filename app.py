97d4a8e64e77940293ebb9da426a6044cea7fdfe
Nick Cross <njcross1990@gmail.com>
2024-12-18 16:01:02 -0600

    pagination and auth

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #app.py
+from functools import wraps
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -7,11 +8,16 @@ from sqlalchemy import Float, ForeignKey, Table, String, Column, UniqueConstrain
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 from typing import List, Optional
-import os, datetime
-import json
+import uuid
+import json, jwt
+from datetime import datetime, timedelta, timezone
+from  werkzeug.security import generate_password_hash, check_password_hash
 
 # Initialize Flask app
 app = Flask(__name__)
+
+#JWT token 
+app.config['SECRET_KEY'] = 'my super secret key'
 
 # MySQL database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:11Oval11@localhost/flask_api_db'
@@ -38,9 +44,11 @@ order_product = Table(
 class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
+    public_id: Mapped[Optional[str]] = mapped_column(String(200), unique=True)
     name: Mapped[str] = mapped_column(String(30))
     address: Mapped[str] = mapped_column(String(95))
-    email: Mapped[Optional[str]] = mapped_column(String(200), unique=True)
+    email: Mapped[str] = mapped_column(String(200), unique=True)
+    password: Mapped[str] = mapped_column(String(200))
 		
 	#One-to-Many relationship from this User to a List of Pet Objects
     orders: Mapped[List["Order"]] = relationship(back_populates="user", cascade="all, delete-orphan")
@@ -49,7 +57,7 @@ class Order(Base):
     __tablename__ = "orders"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    order_date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    order_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
 		
     #Many-to-Many relationship
@@ -91,37 +99,119 @@ orders_schema = OrderSchema(many=True)
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 
+# decorator for verifying the JWT
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+  
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            query = select(User).where(User.public_id == data['public_id'])
+            current_user = db.session.execute(query).scalars().first()
+        except:
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users context to the routes
+        return  f(current_user, *args, **kwargs)
+  
+    return decorated
+
 #======== User Routes =========
+# route for logging user in
+@app.route('/login', methods =['POST'])
+def login():
+    # creates dictionary of form data
+    auth = request.json
+  
+    if not auth or not auth.get('email') or not auth.get('password'):
+        # returns 401 if any email or / and password is missing
+        return jsonify({"message": f"Missing required fields"}), 401
+  
+    query = select(User).where(User.email == auth.get('email'))
+    user = db.session.execute(query).scalars().first()
+    if not user:
+        # returns 401 if user does not exist
+        return jsonify({"message": f"User doesn't exist"}), 401
+  
+    if check_password_hash(user.password, auth.get('password')):
+        # generates the JWT Token
+        token = jwt.encode({
+            'public_id': user.public_id,
+            'exp' : datetime.now(timezone.utc) + timedelta(minutes = 30)
+        }, app.config['SECRET_KEY'])
+  
+        return jsonify({'token' : token}), 201
+    # returns 403 if password is wrong
+    return jsonify({"message": f"Couldn't verify password"}), 403
+
 @app.route('/users', methods=['POST'])
-def create_user():
+@token_required
+def create_user(current_user):
     try:
         user_data = user_schema.load(request.json)
     except ValidationError as e:
         return jsonify(e.messages), 400
     
     try:
-        new_user = User(name=user_data['name'], address=user_data['address'], email=user_data['email'])
+        new_user = User(name=user_data['name'], address=user_data['address'], email=user_data['email'], password=generate_password_hash(user_data['password']), public_id=str(uuid.uuid4()))
         db.session.add(new_user)
         db.session.commit()
     except exc.IntegrityError as e:
         return jsonify({"message": f"Duplicate email {user_data['email']}"}), 400
 
-    return user_schema.jsonify(new_user), 201
+    return user_schema.jsonify({
+            'public_id': new_user.public_id,
+            'name' : new_user.name,
+            'email' : new_user.email,
+            'address' : new_user.address
+        }), 201
 
 @app.route('/users', methods=['GET'])
-def get_users():
-    query = select(User)
-    users = db.session.execute(query).scalars().all()
-
-    return users_schema.jsonify(users), 200
+@token_required
+def get_users(current_user):
+    page = request.args.get('page')
+    query = select(User).order_by(User.id)
+    if page:
+        users = db.paginate(query, page=int(page), per_page=20, error_out=False).items
+    else:
+        users = db.session.execute(query).scalars().all()
+    # converting the query objects
+    # to list of jsons
+    output = []
+    for user in users:
+        # appending the user data json 
+        # to the response list
+        output.append({
+            'public_id': user.public_id,
+            'name' : user.name,
+            'email' : user.email,
+            'address' : user.address
+        })
+    return users_schema.jsonify(output), 200
 
 @app.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
+@token_required
+def get_user(current_user, id):
     user = db.session.get(User, id)
-    return user_schema.jsonify(user), 200
+    return user_schema.jsonify({
+            'public_id': user.public_id,
+            'name' : user.name,
+            'email' : user.email,
+            'address' : user.address
+        }), 200
 
 @app.route('/users/<int:id>', methods=['PUT'])
-def update_user(id):
+@token_required
+def update_user(current_user, id):
     user = db.session.get(User, id)
 
     if not user:
@@ -135,12 +225,20 @@ def update_user(id):
     user.name = user_data['name']
     user.email = user_data['email']
     user.address = user_data['address']
+    user.password = generate_password_hash(user.password['password'])
+    user.public_id=str(uuid.uuid4())
 
     db.session.commit()
-    return user_schema.jsonify(user), 200
+    return user_schema.jsonify({
+            'public_id': user.public_id,
+            'name' : user.name,
+            'email' : user.email,
+            'address' : user.address
+        }), 200
 
 @app.route('/users/<int:id>', methods=['DELETE'])
-def delete_user(id):
+@token_required
+def delete_user(current_user, id):
     user = db.session.get(User, id)
 
     if not user:
@@ -165,8 +263,12 @@ def create_product():
 
 @app.route('/products', methods=['GET'])
 def get_products():
-    query = select(Product)
-    products = db.session.execute(query).scalars().all()
+    page = request.args.get('page')
+    query = select(Product).order_by(Product.id)
+    if page:
+        products = db.paginate(query, page=int(page), per_page=20, error_out=False).items
+    else:
+        products = db.session.execute(query).scalars().all()
 
     return products_schema.jsonify(products), 200
 
@@ -205,6 +307,7 @@ def delete_product(id):
     return jsonify({"message": f"succefully deleted product {id}"}), 200
 
 #  ======== Order Routes ========
+# "order_date": "2021-09-09 15:44:15.81785"
 @app.route('/orders', methods=['POST'])
 def create_order():
     try:
